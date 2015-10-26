9d0f78a84ebb324af78abc45b6b0d4fb9fc321c8
brentzucker <brentzucker@gmail.com>
2015-10-26 12:03:24 -0400

    New Endpoint: Get Total Occupancy of Specific Buildings and array of Occupancies
    - will use something similar to get total occupancy of specific rooms
    aka floors

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 // To run: node server.js
 
-var hostname = require('os').hostname();
-var port = 8080;
+global.hostname = require('os').hostname();
+global.port = 8080;
 
 /* Configure Express with Node */
 
@@ -10,7 +10,7 @@ var app = express();
 
 /* Run App */
 
-app.listen(port);
+app.listen(global.port);
 
 /* * * * * * * * * * * * *
  * API End Points 
@@ -94,7 +94,7 @@ app.get('/api/locationinfo_allbuildings', function (request, response) {
 
 				// Occupancy returned, count until all are returned
 				var occ = (JSON.parse(occupancy)).occupancy;
-				var json = '{';
+				var json = '"' + b_id + '" : {';
 				json += '"b_id": "' + b_id + '", ';
 				json += '"occupancy": ' + occ + ', ';
 				json += '"name": "' + global.buildings['b_id: ' + b_id] + '"';
@@ -107,12 +107,86 @@ app.get('/api/locationinfo_allbuildings', function (request, response) {
 				// Return json string when all occupancy requests have terminated
 				if (global.count >= global.buildings_length) {
 
-					var final_json = '{"occupancies": [' + global.occupancies + ']}'; 
+					var final_json = '{"occupancies": {' + global.occupancies + '}}'; 
 					response.send(final_json);
 				}
 			});
 		});
 	}
+});
+
+app.get('/api/locationinfo/:str', function (request, response) {
+	var str = request.params.str;
+
+	/* Parse Input String */
+
+	var tokens = request.params.str.split("&");
+	var buildings = [];
+	for (var i = 0; i < tokens.length; i++) {
+
+		if (tokens[i].split("=").length === 2) {
+			if (tokens[i].split("=")[0] === "b_id") {
+
+				// i.e. [b_id, 81-337] or [b_id, 81]
+				var b_id_raw_array = tokens[i].split("=");
+
+				// i.e. [81] or [81, 337]
+				var b_id_array = b_id_raw_array[1].split("-");
+
+				if (b_id_array.length == 1) {
+					// request just b_id
+					var building = {'b_id': b_id_array[0]};
+				} else if (b_id_array.length == 2) {
+					// request b_id and room
+					var building = {'b_id': b_id_array[0],
+									'room': b_id_array[1]};
+				} else {
+					// bad request
+					var building = {};
+				}
+				buildings.push(building);
+			} else {
+				// bad request
+				response.send('Bad Request: ' + str);
+			}
+		} else {
+			// bad request
+			response.send('Bad Request: ' + str);
+		}
+	}
+
+	/* Request Location Info for each building
+	 * TODO: Reuqest location info for room */
+	var uri = '/api/locationinfo_allbuildings';
+	var url = 'http://' + global.hostname + ':' + global.port + uri;
+
+	global.http.get(url, function(res) {
+
+		var data = '';
+		res.on('data', function (chunk) {
+			data += chunk;
+		});
+
+		res.on("end", function() {
+			var occ_list = JSON.parse(data);
+
+			var occupancies = [];
+			var total_occupancy = 0;
+			for (var i = 0; i < buildings.length; i++) {
+
+				var b_id = buildings[i].b_id;
+				var occupancy = occ_list.occupancies[buildings[i].b_id].occupancy
+				var json = {b_id : occupancy};
+				occupancies.push(json);
+
+				total_occupancy += occupancy;
+			}
+			var final_json = {};
+			final_json['total_occupancy'] = total_occupancy;
+			final_json['occupancies'] = occupancies;
+			response.send(final_json);
+		});
+	});
 });
 
 /* * * * * * * * * * * * *
