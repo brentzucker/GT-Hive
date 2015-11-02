a0e16ad18cc23986db77e2e635b31e390e3703b9
Daniel Chen <daniel.xia.chen@gmail.com>
2015-11-02 13:05:37 -0800

    Configure app to handle CORS requests

--- Server/server.js
+++ Server/server.js
@@ -13,6 +13,14 @@ global.buildings_rooms = JSON.parse(getBuildingRoomsTextFile());
 var express = require('express');
 var app = express();
 
+// Configure app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
 /* Run App */
 
 app.listen(global.port);
