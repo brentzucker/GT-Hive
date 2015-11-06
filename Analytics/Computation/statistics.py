6e326898dd8eb348c1e0acb61205cc758820a07e
brentzucker <brentzucker@gmail.com>
2015-11-06 13:57:51 -0500

    Calculate median for each day

--- Analytics/Computation/statistics.py
+++ Analytics/Computation/statistics.py
@@ -46,16 +46,9 @@ for h in range(0, 24):
 				buildings[b_id][date][hour] = {}
 				buildings[b_id][date][hour]['count_users_unique'] = 0
 				buildings[b_id][date][hour]['count_users'] = 0
-
-# Move all hours into 'hours' dict
-for b_id in buildings.keys():
-	for date in buildings[b_id].keys():
-		hours = {}
-		for hour in buildings[b_id][date].keys():
-			if hour != 'total':
-				hours[hour] = buildings[b_id][date][hour]
-				del buildings[b_id][date][hour]
-		buildings[b_id][date]['hours'] = hours
+			if 'total' not in buildings[b_id][date]:
+				buildings[b_id][date]['total'] = {}
+				buildings[b_id][date]['total']['count_users_unique'] = 0
 
 # Move all dates into 'dates' dict
 for b_id in buildings.keys():
@@ -64,6 +57,16 @@ for b_id in buildings.keys():
 		dates[date] = buildings[b_id][date]
 		del buildings[b_id][date]
 		buildings[b_id]['dates'] = dates
+
+# Move all hours into 'hours' dict
+for b_id in buildings.keys():
+	for date in buildings[b_id]['dates'].keys():
+		hours = {}
+		for hour in buildings[b_id]['dates'][date].keys():
+			if hour != 'total':
+				hours[hour] = buildings[b_id]['dates'][date][hour]
+				del buildings[b_id]['dates'][date][hour]
+		buildings[b_id]['dates'][date]['hours'] = hours
 
 # Calculate Max for each day
 for b_id in buildings.keys():
@@ -74,6 +77,26 @@ for b_id in buildings.keys():
 				max_users_unique = buildings[b_id]['dates'][date]['hours'][hour]['count_users_unique']
 		buildings[b_id]['dates'][date]['max_users_unique'] = max_users_unique
 
+# Calculate Min for each day
+for b_id in buildings.keys():
+	for date in buildings[b_id]['dates'].keys():
+		min_users_unique = buildings[b_id]['dates'][date]['max_users_unique']
+		for hour in buildings[b_id]['dates'][date]['hours'].keys():
+			if buildings[b_id]['dates'][date]['hours'][hour]['count_users_unique'] < min_users_unique:
+				min_users_unique = buildings[b_id]['dates'][date]['hours'][hour]['count_users_unique']
+		buildings[b_id]['dates'][date]['min_users_unique'] = min_users_unique
+
+# Calculate Median for each day
+for b_id in buildings.keys():
+	for date in buildings[b_id]['dates'].keys():
+		median = 0
+		users_unique = []
+		for hour in buildings[b_id]['dates'][date]['hours'].keys():
+			users_unique.append(buildings[b_id]['dates'][date]['hours'][hour]['count_users_unique'])
+		users_unique.sort()
+		median = users_unique[len(users_unique) / 2]
+		buildings[b_id]['dates'][date]['median_users_unique'] = median
+
 # Calculate Max for each building
 for b_id in buildings.keys():
 	max_users_unique = -1
@@ -82,14 +105,26 @@ for b_id in buildings.keys():
 			max_users_unique = buildings[b_id]['dates'][date]['max_users_unique']
 	buildings[b_id]['max_users_unique'] = max_users_unique
 
+# Calculate Min for each building
+for b_id in buildings.keys():
+	min_users_unique = buildings[b_id]['max_users_unique']
+	for date in buildings[b_id]['dates'].keys():
+		if buildings[b_id]['dates'][date]['min_users_unique'] < min_users_unique:
+			min_users_unique = buildings[b_id]['dates'][date]['min_users_unique']
+	buildings[b_id]['min_users_unique'] = min_users_unique
+
 for b_id in sorted(buildings.keys()):
 	print '\'' + b_id + '\'' 
 	print 'max: ' + str(buildings[b_id]['max_users_unique'])
+	print 'min: ' + str(buildings[b_id]['min_users_unique'])
 	for date in sorted(buildings[b_id]['dates'].keys()):
 		print '\t' + date
 		print '\t\t' + 'max: ' + str(buildings[b_id]['dates'][date]['max_users_unique'])
+		print '\t\t' + 'min: ' + str(buildings[b_id]['dates'][date]['min_users_unique'])
+		print '\t\t' + 'median: ' + str(buildings[b_id]['dates'][date]['median_users_unique'])
 		for hour in sorted(buildings[b_id]['dates'][date]['hours'].keys()):
 			print '\t\t' + hour + ': ' + str(buildings[b_id]['dates'][date]['hours'][hour]['count_users_unique'])
+		print '\t\t' + 'total: ' + str(buildings[b_id]['dates'][date]['total']['count_users_unique'])
 
 
 
