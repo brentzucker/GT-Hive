07a28b946e43d083909b6b1af3be80e929c28810
brentzucker <brentzucker@gmail.com>
2015-11-06 11:45:00 -0500

    Cleaned Data (Buildings, dates, hours)

--- Analytics/Computation/statistics.py
+++ Analytics/Computation/statistics.py
@@ -8,10 +8,44 @@ txt = f.read()
 
 buildings = json.loads(txt)
 
-# Remove Mac Addresses
+# Clean up buildings
 for b_id in buildings.keys():
+	# Remove Mac Addresses
 	if len(b_id.split(":")) == 6:
 		del buildings[b_id]
+	# Remove weird buildings (i.e. AP0007.7db6.01b8) 
+	if len(b_id.split(".")) == 3:
+		del buildings[b_id]
+	# Remove Rich246_EarlsOffice
+	if b_id == "Rich246_EarlsOffice,":
+		del buildings[b_id]
+	# Remove Survey
+	if b_id == "Survey":
+		del buildings[b_id]
+
+# Clean dates
+# If no one was in a building on a day, the date log does not exist
+month = "04"
+year = "2015"
+for d in range(1, 31):
+	day = str(d).zfill(2)
+	date = month +'-'+ day +'-'+ year
+
+	for b_id in buildings.keys():
+		if date not in buildings[b_id].keys():
+			buildings[b_id][date] = {}	
+
+# Clean hours
+# If no one was in a building on an hour, the hour log does not exist
+for h in range(0, 24):
+	hour = str(h).zfill(2)
+
+	for b_id in buildings.keys():
+		for date in buildings[b_id].keys():
+			if hour not in buildings[b_id][date].keys():
+				buildings[b_id][date][hour] = {}
+				buildings[b_id][date][hour]['count_users_unique'] = 0
+				buildings[b_id][date][hour]['count_users'] = 0
 
 for b_id in sorted(buildings.keys()):
 	print b_id 
