e043d7474c446bc52a43bdba4b49f34cc501db00
brentzucker <brentzucker@gmail.com>
2015-11-11 10:30:26 -0500

    Building Details View: Made Line Chart for Occupancies

--- MobileApp/GTHive/app/src/main/java/hive/mas/com/gthive/BuildingFragment.java
+++ MobileApp/GTHive/app/src/main/java/hive/mas/com/gthive/BuildingFragment.java
@@ -1,11 +1,20 @@
 package hive.mas.com.gthive;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bvz on 11/2/15.
@@ -50,6 +59,19 @@ public class BuildingFragment extends android.support.v4.app.Fragment {
         mOccupancyTextView = (TextView) v.findViewById(R.id.occupancy);
         mOccupancyTextView.setText("" + mBuilding.getOccupancy());
 
+
+        /* Draw Line Graph */
+
+        // List<Integer> todaysCrowdValues = mBuilding.getTodaysCrowdValues();
+        // List<Integer> rodCrowdValues = mBuilding.getRodCrowdValues();
+        // temporarily using random numbers
+        List<Integer> todaysCrowdValues = new ArrayList<>();
+        List<Integer> rodCrowdValues = new ArrayList<>();
+        for (int i = 0; i <= 12; i++) todaysCrowdValues.add( (int)(100 * (Math.random() * 10)));
+        for (int i = 0; i <= 12; i++) rodCrowdValues.add( (int)(100 * (Math.random() * 10)));
+
+        drawLineChart(v, todaysCrowdValues, rodCrowdValues);
+
         // Temporarily use fake numbers for best times
         mFirstBestTimeTextView = (TextView) v.findViewById(R.id.first_best_time_text_view);
         mFirstBestTimeTextView.setText("8am");
@@ -75,5 +97,46 @@ public class BuildingFragment extends android.support.v4.app.Fragment {
         }
 
         return v;
+    }
+
+    public void drawLineChart(View v, List<Integer> todaysCrowdValues, List<Integer> rodCrowdValues) {
+
+        /* Create data entries1 and labels for todaysCrowd Values*/
+        ArrayList<Entry> entries1 = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<>();
+        for (int hour = 0; hour < todaysCrowdValues.size(); hour++) {
+            entries1.add(new Entry(todaysCrowdValues.get(hour), hour));
+            labels.add("" + hour);
+        }
+
+        /* Create data entries2 and labels for rodCrowdValues Values*/
+        ArrayList<Entry> entries2 = new ArrayList<>();
+        for (int hour = 0; hour < rodCrowdValues.size(); hour++) {
+            entries2.add(new Entry(rodCrowdValues.get(hour), todaysCrowdValues.size() + 12));
+            labels.add("" + (hour + 12));
+        }
+
+        // Create dataset from data entries1
+        LineDataSet dataset1 = new LineDataSet(entries1, "Todays Crowd Values");
+        LineDataSet dataset2 = new LineDataSet(entries2, "Predicted Crowd Values");
+
+        // Set the color for this dataset
+        dataset1.setColor(Color.rgb(0, 37, 76)); // GT Navy
+        dataset2.setColor(Color.rgb(238, 178, 17)); // Buzz Gold
+
+        /* Create the chart */
+        LineChart chart = (LineChart) v.findViewById(R.id.chart);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(dataset1);
+        dataSets.add(dataset2);
+
+        LineData data = new LineData(labels, dataSets);
+        chart.setData(data);
+
+        chart.setDescription("Today's Crowd Level");
+
+        // animations
+        chart.animateY(1000);
     }
 }
