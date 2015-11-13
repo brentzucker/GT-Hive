2160ca0c809f3e11d7e1f096a39641e0298a0ad1
brentzucker <brentzucker@gmail.com>
2015-11-13 12:10:07 -0500

    removed okhttp

--- MobileApp/GTHive/app/src/main/java/hive/mas/com/gthive/Campus.java
+++ MobileApp/GTHive/app/src/main/java/hive/mas/com/gthive/Campus.java
@@ -3,11 +3,6 @@ package hive.mas.com.gthive;
 import android.content.Context;
 import android.util.Log;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,7 +12,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 /**
